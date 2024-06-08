a3317dbf69bfe3155902e31b5cca5446ad716a90
Losthunter100 <btech10170.23@bitmesra.ac.in>
2024-06-08 09:35:35 +0000

    improve

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+// alert(3+3)  we're using nodejs, not browser
+console.log(3
+    +3
+); // code readibility should be high
+console.log("Hitesh")
+// 2 documentations tc39(OG) and mdn
+let name= "hitesh"
+let age = 18
+let isLoggedIn= false
+let state;
+// number -> 2 to power 53
+// bigint
+// string = ""
+// boolean -> true/false
+// null -> standalone value
+// undefined
+// symbol -> uniquex   
+
+//  object
+console.log(typeof null)
+console.log(typeof undefined);
+
+
