aee0e9f812674e783b7c1fabd1a2a2905bb8ee2d
Losthunter100 <btech10170.23@bitmesra.ac.in>
2024-06-08 09:59:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber= Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); //  NaN -> Not a Number 
+
+// let score1 = null
+// console.log(typeof score1);
+// console.log(typeof(score1));
+// let valueInNumber1= Number(score1);
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1); //  0 
+
+// let score2 = undefined
+// console.log(typeof score2);
+// console.log(typeof(score2));
+// let valueInNumber2= Number(score2);
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2); //  NaN -> Not a Number 
+
+//  "33"-> 33
+// "33abc" ->  NaN
+//  true -> 1 ; false -> 0
+
+let isLoggedIn= 1
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// ""-> false
+// "hitesh"-> true
+let someNumber= 33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
